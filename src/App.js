59f19f0d4776a6dc1f38f80fbda52764cb98ae7d
Sophie Lewis <sophielewis@protonmail.com>
2021-06-30 16:00:49 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+const api = {
+  key: 'process.env.REACT_APP_API_KEY',
+  base: "https://api.weatherbit.io/v2.0/current?city=Sydney&country=Australia&key="
+}
 
 function App() {
+  const [weather, setWeather] = useState({});
+
+const search = () => {
+      fetch(`${api.base}${api.key}`, { mode: "cors" })
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={search}>Get Weather Data</button>
+      {(typeof weather.data != 'undefined') ? (
+        <div className="weather-data">
+          <p>{weather.data[0].temp}</p>
+          </div>
+        ) : ('')}
+        
     </div>
   );
 }
